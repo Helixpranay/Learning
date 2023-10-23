30308828fd3cfcab8ec6068a9ef435fc916191a8
Helixpranay <pranaykumar31399@gmail.com>
2023-10-23 15:42:22 +0530

    Update ReactHook2.tsx

--- react-hooks/usestate-useeffect/src/react-hooks/ReactHook2.tsx
+++ react-hooks/usestate-useeffect/src/react-hooks/ReactHook2.tsx
@@ -14,7 +14,11 @@ function ReactHook2() {
         setTimeout(() => {
             fetch('https://pokeapi.co/api/v2/pokemon/')
                 .then((response) => response.json())
-                    .then((data: ApiResponse) => setData(data));
+                    .then((data: ApiResponse) => {
+                        setData(data);
+                        const names = data.results.map((result) => result.name); 
+                        console.log('names',names); //names (20) ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate']
+                    });
         },2000)
     },[])
 
