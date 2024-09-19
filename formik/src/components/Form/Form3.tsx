349b9f80f90330b5db03836040b5f6a5a77c34b4
Sindhujangam <pranaykumar31399@gmail.com>
2024-09-19 09:22:40 +0530

    formik: custom component

--- formik/src/components/Form/Form3.tsx
+++ formik/src/components/Form/Form3.tsx
@@ -7,12 +7,34 @@ import * as Yup from "yup";
 
 const FormThree = () => {
   const formikProps = {
-    initialValues: { firstname: "" },
+    initialValues: { firstname: "", color: "", lastname: "" },
     validationSchema: Yup.object({
       firstname: Yup.string().required("Sorry ! This is required"),
+      lastname: Yup.string().required("Sorry ! This is required"),
     }),
     onSubmit: (values: any) => console.log(values),
   };
+
+  /* custom comopnent benefits => re-usable code */
+  const lastnamecomponent = ({
+    field, /// {name, value, onChange, onBlue}
+    form: { touched, errors },
+    ...props
+  }: any) => (
+    <>
+      <label htmlFor={field.name}>{props.labelName}</label>
+      <input
+        type="text"
+        className="form-control"
+        placeholder={props.placeholder}
+        {...field}
+      />
+      {errors[field.name] && touched[field.name] ? (
+        <span>{errors[field.name]}</span>
+      ) : null}
+    </>
+  );
+
   return (
     <div className="container">
       <div className="col-md-12 mt-5">
@@ -27,6 +49,15 @@ const FormThree = () => {
                 <span>{formik.errors.firstname}</span>
               ) : null}
               <hr className="mb-4" />
+
+              <Field
+                name="lastname"
+                component={lastnamecomponent}
+                placeholder="Last name"
+                labelName="Enter your last name"
+              />
+              <hr className="mb-4" />
+
               <label htmlFor="color">Color</label>
               <Field as="select" name="color" className="custom-control">
                 <option value="red">Red</option>
